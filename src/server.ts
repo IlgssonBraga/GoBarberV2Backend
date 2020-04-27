4fbd1ca862457f3ecf30cc159eb2f0362ec744a5
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-04-27 20:43:00 -0300

    feat: Handling errors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import routes from './routes';
 import './database';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 const app = express();
 
@@ -11,6 +13,22 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 
 app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+        return res.status(err.statusCode).json({
+            status: 'error',
+            message: err.message,
+        });
+    }
+
+    console.error(err);
+
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+    });
+});
 
 app.listen(3333, () => {
     console.log('Backend iniciado!');
