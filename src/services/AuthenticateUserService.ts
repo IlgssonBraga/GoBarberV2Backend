ee740afe231a62f555821e806c831a3b86fe44d4
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-04-27 08:04:16 -0300

    feat: json web token

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,44 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import User from '../models/User';
+
+interface Request {
+    email: string;
+    password: string;
+}
+
+interface Response {
+    user: User;
+    token: string;
+}
+
+class AuthenticateUserService {
+    public async execute({ email, password }: Request): Promise<Response> {
+        const userRepository = getRepository(User);
+
+        const user = await userRepository.findOne({ where: { email } });
+
+        if (!user) {
+            throw new Error('Incorrect e-mail or password.');
+        }
+
+        const passwordMatched = await compare(password, user.password);
+
+        if (!passwordMatched) {
+            throw new Error('Incorrect e-mail or password.');
+        }
+
+        const token = sign({}, '698dc19d489c4e4db73e28a713eab07b', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+
+        return {
+            user,
+            token,
+        };
+    }
+}
+
+export default AuthenticateUserService;
