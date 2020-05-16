84f7c3b4b7c37544b6acbecaa139da1a77894bf4
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-05-16 18:10:56 -0300

    feat: refatorando testes

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -2,11 +2,9 @@ import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import AuthenticateUserService from './AuthenticateUserService';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
-import CreateUserService from './CreateUserService';
 
 let fakeUsersRepository: FakeUsersRepository;
 let fakeHashProvider: FakeHashProvider;
-let createUserService: CreateUserService;
 let authenticateUserService: AuthenticateUserService;
 
 describe('AuthenticateUser', () => {
@@ -14,10 +12,6 @@ describe('AuthenticateUser', () => {
         fakeUsersRepository = new FakeUsersRepository();
         fakeHashProvider = new FakeHashProvider();
 
-        createUserService = new CreateUserService(
-            fakeUsersRepository,
-            fakeHashProvider,
-        );
         authenticateUserService = new AuthenticateUserService(
             fakeUsersRepository,
             fakeHashProvider,
@@ -25,7 +19,7 @@ describe('AuthenticateUser', () => {
     });
 
     it('should be able to authenticate', async () => {
-        const user = await createUserService.execute({
+        const user = await fakeUsersRepository.create({
             name: 'Ilgsson',
             email: 'ilgsson@gmail.com',
             password: '123456',
@@ -50,7 +44,7 @@ describe('AuthenticateUser', () => {
     });
 
     it('should not be able to authenticate with wrong password', async () => {
-        await createUserService.execute({
+        await fakeUsersRepository.create({
             name: 'Ilgsson',
             email: 'ilgsson@gmail.com',
             password: '123456',
